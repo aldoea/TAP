f17ff35d4650c64ba24754b20066ded26bdbcc00
aldoea <aldo.ea@hotmail.com>
2019-02-17 18:47:08 -0600

    Calculadora - @aldoea: Calculater working properly after modifications after error detected on thuesday class revision

--- src/sample/Eventos/EventosCalculadora.java
+++ src/sample/Eventos/EventosCalculadora.java
@@ -9,8 +9,7 @@ public class EventosCalculadora implements EventHandler {
     TextField txtOperacion;
     static String operacionSimbol;
     static double valorAnterior, ultimoValor, resultado;
-    static boolean operacionFlag = false;
-    static boolean soloDecimalFlag = false;
+    static boolean operacionFlag, soloDecimalFlag, errorFlag;
 
     public EventosCalculadora(String valor, TextField textOp) {
         this.valor = valor;
@@ -19,6 +18,7 @@ public class EventosCalculadora implements EventHandler {
 
     @Override
     public void handle(Event event) {
+        if(errorFlag) limpiar();
         switch (valor) {
             default:
                 if(txtOperacion.getText().equals("0")) {
@@ -26,8 +26,8 @@ public class EventosCalculadora implements EventHandler {
                 }
                 if(operacionFlag && !soloDecimalFlag){
                     txtOperacion.setText("");
-                    operacionFlag = false;
                     ultimoValor = 0;
+                    operacionFlag = false;
                 }
                 txtOperacion.appendText(valor);
                 break;
@@ -59,55 +59,64 @@ public class EventosCalculadora implements EventHandler {
                 if(ultimoValor == 0) { // Para asegurar que se opere el ultimo valor si no se realiza una operación nueva.
                     ultimoValor = Double.parseDouble(txtOperacion.getText());
                 }
-                hacerOperacion();
+                if(operacionSimbol != null) {
+                    hacerOperacion();
+                    valorAnterior = resultado;
+                }else {
+                    resultado = Double.parseDouble(txtOperacion.getText());
+                }
                 if(resultado == 0) {
                     txtOperacion.setText("0");
-                }else if(Double.isNaN(resultado)){
-                    txtOperacion.setText("ERROR: Loop");
+                }else if(Double.isNaN(resultado) || Double.isInfinite(resultado)){
+                    errorFlag = true;
+                    txtOperacion.setText("ERROR LOOP: " + resultado);
                 }else{
                     txtOperacion.setText(Double.toString(resultado));
                 }
-                valorAnterior = resultado;
                 break;
             case ".":
-                if(txtOperacion.getText().equals("0") || (operacionFlag && !soloDecimalFlag) ) {
+                // Agregar un cero-punto en caso de que no se indique el cero antes.
+                if(txtOperacion.getText().equals("0") || ((operacionSimbol != null) && operacionFlag)) {
                     soloDecimalFlag = true;
-                    ultimoValor = 0;
                     txtOperacion.setText("0.");
-                }else{
-                    // Prevenir que se agregue otro punto.
-                    if(!soloDecimalFlag) txtOperacion.appendText(valor);
+                    //ultimoValor = 0;
+                }else if(!soloDecimalFlag){ // Prevenir que se agregue otro punto.
+                    txtOperacion.appendText(valor);
+                    soloDecimalFlag = true;
                 }
                 break;
             case "Ç":
-                ultimoValor = 0;
-                valorAnterior = 0;
-                resultado = 0;
-                operacionFlag = false;
-                soloDecimalFlag = false;
-                operacionSimbol = null;
-                txtOperacion.setText("0");
+                limpiar();
         } // END switch
     } // END handle
 
     public void hacerOperacion() {
         switch (operacionSimbol){
             case "+":
-                System.out.println("Valor anterior " + valorAnterior + " + " + ultimoValor + " = " + (valorAnterior + ultimoValor));
                 resultado = valorAnterior + ultimoValor;
                 break;
             case "-":
-                System.out.println("Valor anterior " + valorAnterior + " - " + ultimoValor + " = " + (valorAnterior - ultimoValor));
                 resultado = valorAnterior - ultimoValor;
                 break;
             case "*":
-                System.out.println("Valor anterior " + valorAnterior + " * " + ultimoValor + " = " + (valorAnterior * ultimoValor));
                 resultado = valorAnterior * ultimoValor;
                 break;
             case "/":
-                System.out.println("Valor anterior " + valorAnterior + " /" + ultimoValor + " = " + (valorAnterior /  ultimoValor));
                 resultado = valorAnterior / ultimoValor;
                 break;
         }
+        operacionFlag = true;
+        soloDecimalFlag = false;
     } //
+
+    public void limpiar() {
+        ultimoValor = 0;
+        valorAnterior = 0;
+        resultado = 0;
+        operacionFlag = false;
+        soloDecimalFlag = false;
+        errorFlag = false;
+        operacionSimbol = null;
+        txtOperacion.setText("0");
+    }
 } // END EventosCalculadora class
