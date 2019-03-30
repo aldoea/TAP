33ea78ab3d779d319b1e8a97fc21b642f2833584
aldoea <aldo.ea@hotmail.com>
2019-03-30 17:29:16 -0600

    TAP/Threads - @aldoea: Avance en vista de menu de restaurant, siguiente: crear platillos y sus contenedores.

--- src/sample/Vistas/RestauranteMenu.java
+++ src/sample/Vistas/RestauranteMenu.java
@@ -2,45 +2,68 @@ package sample.Vistas;
 
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class RestauranteMenu extends Stage {
 
     private Scene escena;
-    private BorderPane mainBorderPane;
     private TabPane tabPane;
-    //private Tab[] tabs;
     private String[] tabsNames = {
-        "Desayunos", "Comidas", "Bebidas", "Ligth"
+        "Entradas", "Pastas", "Carnes", "Bebidas"
     };
+    private VBox vBoxMain;
+    private ArrayList<HBox> platillos = new ArrayList<>(tabsNames.length);
+    private HBox[] hBoxContPlati = new HBox[tabsNames.length];
+    private Button btnConfirmar;
+
     public RestauranteMenu() {
         CrearGUI();
-        escena = new Scene(mainBorderPane,400, 400);
-        setTitle("Menu La Cafe 3");
+        escena = new Scene(vBoxMain,400, 400);
+        setTitle("Ratatouille");
         setScene(escena);
         setMaximized(true);
         show();
     }
 
+    private Color[] colors = {
+            Color.LIGHTSTEELBLUE,
+            Color.LAWNGREEN,
+            Color.TOMATO,
+            Color.SLATEBLUE,
+            Color.AQUA
+    };
+
     private void CrearGUI() {
-        mainBorderPane = new BorderPane();
-        mainBorderPane.getStylesheets().add(getClass().getResource("../CSS/Bootstrap3.css").toExternalForm());
+        vBoxMain = new VBox();
+        vBoxMain.getStylesheets().add(getClass().getResource("../CSS/Bootstrap3.css").toExternalForm());
+        vBoxMain.getStylesheets().add(getClass().getResource("../CSS/restaurant.css").toExternalForm());
+        btnConfirmar = new Button("Confirmar");
         tabPane = new TabPane();
-        tabPane.setTabMinHeight(100);
-        tabPane.setTabMinWidth(200);
         for(int index = 0;index < tabsNames.length; index++) {
             Tab newTab = new Tab();
             newTab.setText(tabsNames[index]);
-            BorderPane dishesPane = new BorderPane();
-            dishesPane.setCenter(new Rectangle(200,200, Color.LIGHTSTEELBLUE));
-            newTab.setContent(dishesPane);
+            crearPlatillos(index);
+            newTab.setContent(hBoxContPlati[index]);
             newTab.closableProperty().setValue(false);
             tabPane.getTabs().add(newTab);
         }
-        mainBorderPane.setCenter(tabPane);
+        vBoxMain.getChildren().addAll(new Label("Agregue los platillos"), tabPane, btnConfirmar);
+    }
+
+    private void crearPlatillos(int index) {
+        hBoxContPlati[index] = new HBox();
+        for (int i = 0; i < 5; i++) {
+            Rectangle t = new Rectangle(200,300, colors[i]);
+            t.setStyle("-fx-padding: 0px, 30px, 0px, 30px");
+            hBoxContPlati[index].getChildren().add(t);
+            hBoxContPlati[index].getStyleClass().add("test-class");
+        }
+        platillos.add(hBoxContPlati[index]);
     }
 }
